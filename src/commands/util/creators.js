0c69f30e5e2a4d462a7e9a3c7e2a7ac20ee90518
gonoahwhere <noahcrowley017@gmail.com>
2026-07-16 22:26:54 +0100

    Added creators render

---
+++ src/commands/util/creators.js
@@ -0,0 +1,47 @@
+import { SlashCommandBuilder, MessageFlags, AttachmentBuilder } from 'discord.js';
+import { CREATOR_DETAILS } from '../../data/creatorKeys.js';
+import { renderCreators } from '../../renders/renderCreators.js';
+import PlayerProfile from '../../models/player.js';
+
+export default {
+    devOnly: false,
+    cooldown: 5,
+    category: 'Util',
+    data: new SlashCommandBuilder()
+        .setName('creators')
+        .setDescription('View the creators behind my creation!'),
+    async execute (interaction) {
+        await interaction.deferReply();
+
+        const creators = await Promise.all(
+            CREATOR_DETAILS.map(async (creator) => {
+                let avatarUrl = null;
+                let displayName = creator.name;
+
+                try {
+                    const user = await interaction.client.users.fetch(creator.id);
+                    avatarUrl = user.displayAvatarURL({ extension: 'png', size: 128 });
+                    displayName = user.username || creator.name;
+                } catch (_) {
+                    // User couldn't be fetched - fallback silently
+                }
+
+                return {
+                    ...creator,
+                    name: displayName,
+                    avatarUrl,
+                };
+            })
+        );
+
+        const viewerProfile = interaction.playerProfile;
+        const buffer = await renderCreators({
+            label: 'Meet the Team',
+            creators,
+            viewerProfile,
+        });
+
+        const attachment = new AttachmentBuilder(buffer, { name: 'creators.png' });
+        await interaction.editReply({ files: [attachment] });
+    }
+}
