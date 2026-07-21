417bc46f3c07c20d54dea324d8b556e4bcd2bd7c
null <60427892+null8626@users.noreply.github.com>
2026-07-21 18:09:21 +0700

    refactor: make sprite handler use Promise.all

--- src/data/sprites.js
+++ src/data/sprites.js
@@ -2,17 +2,21 @@ import { loadImage } from '@napi-rs/canvas';
 import { readFile } from 'fs/promises';
 import path from 'path';
 
-let sheet
-let sheetData
+let sheet;
+let sheetData;
 
 export async function loadSprites() {
-    sheet = await loadImage(path.join(process.cwd(), 'images', 'sprites', 'spritesheet.png'))
-    sheetData = JSON.parse(await readFile(path.join(process.cwd(), 'images', 'sprites', 'spritesheet.json')))
+    [sheet, sheetData] = await Promise.all([
+        loadImage(path.join(process.cwd(), 'images', 'sprites', 'spritesheet.png')),
+        readFile(path.join(process.cwd(), 'images', 'sprites', 'spritesheet.json'))
+    ]);
+
+    sheetData = JSON.parse(sheetData);
 }
 
 export function getSprite(id) {
     return {
         sheet,
         ...sheetData[id]
-    }
+    };
 }
