30ca5928569b4798eb82725c1e741aa184fce30d
null <60427892+null8626@users.noreply.github.com>
2026-07-30 02:18:47 +0700

    feat: update util/start.js

--- src/commands/util/start.js
+++ src/commands/util/start.js
@@ -2,7 +2,9 @@ import { SlashCommandBuilder, MessageFlags } from 'discord.js';
 import PlayerProfile from '../../models/player.js';
 import { errorEmbed } from '../../utils/embed.js';
 import { renderStandDisplay } from '../../renders/renderStandDisplay.js';
+import { rollInitialEvents } from '../../helpers/weatherEvents.js';
 import logger from '../../utils/logger.js';
+import config from '../../../config.js';
 
 export default {
     devOnly: false,
@@ -23,9 +25,14 @@ export default {
 
         let profile;
         try {
+            const { active, next } = rollInitialEvents();
+            const grantsPremium = interaction.guildId === config.premiumGrantGuildId;
+
             profile = await PlayerProfile.create({
                 discordId: interaction.user.id,
                 username: interaction.user.tag,
+                events: { active, next, history: [] },
+                entitlements: { premium: grantsPremium },
             });
         } catch (err) {
             if (err.code === 11000) {
